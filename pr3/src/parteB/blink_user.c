64d17c708dfbe04be1e13f0be46c66522df3fa14
ergl <ergl@users.noreply.github.com>
2017-11-15 14:16:25 +0100

    Wrap driver with userland api

---
+++ pr3/src/parteB/blink_user.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define BLINK_PATH "/dev/usb/blinkstick0"
+
+typedef FILE blink_dev_t;
+typedef struct {
+    unsigned int led;
+    unsigned int color;
+} blink_msg_t;
+
+blink_dev_t* blink_init();
+int blink_deinit(blink_dev_t* dev);
+int send_to_driver(blink_dev_t* dev, blink_msg_t** msg_l, size_t len);
+
+int main(int argc, char** argv) {
+    int ret;
+    blink_dev_t* blink_device = blink_init();
+    if (blink_device == NULL) {
+        return -1;
+    }
+
+    blink_msg_t* messages[] = {
+        [0] = &(blink_msg_t){.led = 0, .color = 0x000000},
+        [1] = &(blink_msg_t){.led = 1, .color = 0x000011},
+        [2] = &(blink_msg_t){.led = 2, .color = 0x001100},
+        [3] = &(blink_msg_t){.led = 3, .color = 0x110000}
+    };
+
+    ret = send_to_driver(blink_device, messages, 4);
+    blink_deinit(blink_device);
+    return ret;
+}
+
+blink_dev_t* blink_init() {
+    blink_dev_t* dev = fopen(BLINK_PATH, "w");
+    if (dev == NULL) {
+        perror("Can't open blinkstick file");
+        return NULL;
+    } else {
+        return dev;
+    }
+}
+
+int blink_deinit(blink_dev_t* dev) {
+    return fclose(dev);
+}
+
+int blink_send(blink_dev_t* dev, const char* buf) {
+    size_t written;
+    size_t orig_size = strlen(buf);
+    written = fwrite(buf, sizeof(char), strlen(buf), dev);
+    return orig_size == written ? 0 : -1;
+}
+
+#define READ_BUF_LEN 256
+#define s_end(s) ((s) + strlen(s))
+
+int send_to_driver(blink_dev_t* dev, blink_msg_t** msg_l, size_t len) {
+    unsigned int i;
+    int size;
+    char dev_msg[READ_BUF_LEN];
+
+    size = sprintf(dev_msg, "%u:0x%X", msg_l[0]->led, msg_l[0]->color);
+    if (size == -1) {
+        return -1;
+    }
+
+    for (i = 1; i < len; i++) {
+        size = sprintf(s_end(dev_msg), ",%u:0x%X", msg_l[i]->led, msg_l[i]->color);
+        if (size == -1) {
+            return -1;
+        }
+    }
+
+    return blink_send(dev, dev_msg);
+}
