3537d08c0f1fc92a9c96bf6fe205dd877d683e8a
ergl <ergl@users.noreply.github.com>
2017-12-09 13:17:42 +0100

    Prettify

--- pr4/prod_cons_1/prodcons1.c
+++ pr4/prod_cons_1/prodcons1.c
@@ -2,165 +2,169 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/string.h>
-#include <linux/vmalloc.h>
-#include <linux/vmalloc.h>
 #include <asm-generic/uaccess.h>
 #include <asm-generic/errno.h>
 #include <linux/semaphore.h>
 #include <linux/kfifo.h>
 
-
-#define MAX_ITEMS_CBUF	4
-#define MAX_CHARS_KBUF	10
+#define MAX_ITEMS_CBUF 4
+#define MAX_CHARS_KBUF 10
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Productor/Consumidor v1 para LIN");
 MODULE_AUTHOR("Juan Carlos Sáez");
 
 static struct proc_dir_entry *proc_entry;
-static struct kfifo cbuf; /* Buffer circular compartido */
-struct semaphore elementos,huecos; /* Semaforos para productor y consumidor */
-struct semaphore mtx; /* Para garantizar exclusión mutua en acceso a buffer */
 
-static ssize_t prodcons_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
-{
+// Buffer circular compartido
+static struct kfifo cbuf;
 
-	char kbuf[MAX_CHARS_KBUF+1];
-	int val=0;
+// Semaforos para productor y consumidor
+struct semaphore elementos, huecos;
 
-	if ((*off) > 0) /* The application can write in this entry just once !! */
-		return 0;
+// Para garantizar exclusión mutua en acceso a buffer
+struct semaphore mtx;
 
-	if (len > MAX_CHARS_KBUF) {
-		return -ENOSPC;
-	}
-	if (copy_from_user( kbuf, buf, len )) {
-		return -EFAULT;
-	}
+static ssize_t prodcons_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+    char kbuf[MAX_CHARS_KBUF + 1];
+    int val=0;
 
-	kbuf[len] ='\0';
-	*off+=len;            /* Update the file pointer */
+    // The application can only write to this entry once
+    if ((*off) > 0) {
+        return 0;
+    }
 
-	if (sscanf(kbuf,"%i",&val)!=1)
-		return -EINVAL;
+    if (len > MAX_CHARS_KBUF) {
+        return -ENOSPC;
+    }
 
+    if (copy_from_user(kbuf, buf, len)) {
+        return -EFAULT;
+    }
 
-	/* Bloqueo hasta que haya huecos */
-	if (down_interruptible(&huecos))
-		return -EINTR;
+    kbuf[len] = '\0';
+    // Update the file pointer
+    *off += len;
 
-	/* Entrar a la SC */
-	if (down_interruptible(&mtx)) {
-		up(&huecos);
-		return -EINTR;
-	}
+    if (sscanf(kbuf, "%i", &val) != 1) {
+        return -EINVAL;
+    }
 
-	/* Inserción segura en el buffer circular */
-	kfifo_in(&cbuf,&val,sizeof(int));
+    // Bloqueo hasta que haya huecos
+    if (down_interruptible(&huecos)) {
+        return -EINTR;
+    }
 
-	/* Salir de la SC */
-	up(&mtx);
+    // Entrar a la SC
+    if (down_interruptible(&mtx)) {
+        up(&huecos);
+        return -EINTR;
+    }
 
-	/* Incremento del número de elementos (reflejado en el semáforo) */
-	up(&elementos);
+    // Inserción segura en el buffer circular
+    kfifo_in(&cbuf, &val, sizeof(int));
 
-	return len;
+    // Salir de la SC
+    up(&mtx);
+
+    // Incremento del número de elementos (reflejado en el semáforo)
+    up(&elementos);
+
+    return len;
 }
 
 
-static ssize_t prodcons_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
-{
+static ssize_t prodcons_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+    int nr_bytes = 0;
+    int val = 0;
+    int bytes_extracted;
+    char kbuff[32] = "";
 
-	int nr_bytes=0;
-	int val=0;
-	int bytes_extracted;
-	char kbuff[32]="";
+    if ((*off) > 0) {
+        return 0;
+    }
 
-	if ((*off) > 0)
-		return 0;
+    // Bloqueo hasta que haya elementos que consumir
+    if (down_interruptible(&elementos)) {
+        return -EINTR;
+    }
 
-	/* Bloqueo hasta que haya elementos que consumir */
-	if (down_interruptible(&elementos))
-		return -EINTR;
+    // Entrar a la SC
+    if (down_interruptible(&mtx)) {
+        up(&elementos);
+        return -EINTR;
+    }
 
-	/* Entrar a la SC  */
-	if (down_interruptible(&mtx)) {
-		up(&elementos);
-		return -EINTR;
-	}
+    // Extraer el primer entero del buffer
+    bytes_extracted = kfifo_out(&cbuf, &val, sizeof(int));
 
-	/* Extraer el primer entero del buffer */
-	bytes_extracted=kfifo_out(&cbuf,&val,sizeof(int));
+    // Salir de la SC
+    up(&mtx);
 
-	/* Salir de la SC */
-	up(&mtx);
+    // Incremento del número de huecos (reflejado en el semáforo)
+    up(&huecos);
 
-	/* Incremento del número de huecos (reflejado en el semáforo) */
-	up(&huecos);
+    if (bytes_extracted != sizeof(int)) {
+        return -EINVAL;
+    }
 
-	if (bytes_extracted!=sizeof(int))
-		return -EINVAL;
+    // Conversion a cadena de caracteres para el usuario
+    nr_bytes = sprintf(kbuff, "%i\n", val);
 
-	/* Conversion a cadena de caracteres para el usuario */
-	nr_bytes=sprintf(kbuff,"%i\n",val);
+    if (len < nr_bytes) {
+        return -ENOSPC;
+    }
 
-	if (len < nr_bytes)
-		return -ENOSPC;
+    if (copy_to_user(buf, kbuff, nr_bytes)) {
+        return -EINVAL;
+    }
 
-	if (copy_to_user(buf,kbuff,nr_bytes))
-		return -EINVAL;
+    (*off )+= nr_bytes;
 
-	(*off)+=nr_bytes; 
-
-	return nr_bytes;
+    return nr_bytes;
 }
 
 static const struct file_operations proc_entry_fops = {
-	.read = prodcons_read,
-	.write = prodcons_write,
+    .read = prodcons_read,
+    .write = prodcons_write,
 };
 
 
-int init_prodcons_module( void )
-{
+int init_prodcons_module(void) {
+    int retval;
+    // Inicialización del buffer
+    retval = kfifo_alloc(&cbuf, MAX_ITEMS_CBUF * sizeof(int), GFP_KERNEL);
 
-	int retval;
-	/* Inicialización del buffer */
-	retval = kfifo_alloc(&cbuf,MAX_ITEMS_CBUF*sizeof(int),GFP_KERNEL);
+    if (retval) {
+        return -ENOMEM;
+    }
 
-	if (retval)
-		return -ENOMEM;
+    // Semaforo elementos inicializado a 0 (buffer vacío)
+    sema_init(&elementos, 0);
 
-	/* Semaforo elementos inicializado a 0 (buffer vacío) */
-	sema_init(&elementos,0);
+    // Semaforo huecos inicializado a MAX_ITEMS_CBUF (buffer vacío)
+    sema_init(&huecos, MAX_ITEMS_CBUF);
 
-	/* Semaforo huecos inicializado a MAX_ITEMS_CBUF (buffer vacío) */
-	sema_init(&huecos,MAX_ITEMS_CBUF);
+    // Semaforo para garantizar exclusion mutua
+    sema_init(&mtx, 1);
 
-	/* Semaforo para garantizar exclusion mutua */
-	sema_init(&mtx,1);
+    proc_entry = proc_create_data("prodcons", 0666, NULL, &proc_entry_fops, NULL);
+    if (proc_entry == NULL) {
+        kfifo_free(&cbuf);
+        printk(KERN_INFO "Prodcons1: No puedo crear la entrada en proc\n");
+        return -ENOMEM;
+    }
 
-	proc_entry = proc_create_data("prodcons",0666, NULL, &proc_entry_fops, NULL);
+    printk(KERN_INFO "Prodcons1: Cargado el Modulo.\n");
 
-	if (proc_entry == NULL) {
-		kfifo_free(&cbuf);
-		printk(KERN_INFO "Prodcons1: No puedo crear la entrada en proc\n");
-		return  -ENOMEM;
-	}
-
-	printk(KERN_INFO "Prodcons1: Cargado el Modulo.\n");
-
-	return 0;
+    return 0;
 }
 
-
-void exit_prodcons_module( void )
-{
-	remove_proc_entry("prodcons", NULL);
-	kfifo_free(&cbuf);
-	printk(KERN_INFO "Prodcons1: Modulo descargado.\n");
+void exit_prodcons_module(void) {
+    remove_proc_entry("prodcons", NULL);
+    kfifo_free(&cbuf);
+    printk(KERN_INFO "Prodcons1: Modulo descargado.\n");
 }
 
-
-module_init( init_prodcons_module );
-module_exit( exit_prodcons_module );
+module_init(init_prodcons_module);
+module_exit(exit_prodcons_module);
--- pr4/prod_cons_2/prodcons2.c
+++ pr4/prod_cons_2/prodcons2.c
@@ -9,9 +9,8 @@
 #include <linux/semaphore.h>
 #include <linux/kfifo.h>
 
-
-#define MAX_ITEMS_CBUF	4
-#define MAX_CHARS_KBUF	10
+#define MAX_ITEMS_CBUF 4
+#define MAX_CHARS_KBUF 10
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Productor/Consumidor v2.0 para LIN");
@@ -19,182 +18,188 @@ MODULE_AUTHOR("Juan Carlos Sáez");
 
 static struct proc_dir_entry *proc_entry;
 static struct kfifo cbuf;
-struct semaphore prod_queue,cons_queue;
+struct semaphore prod_queue, cons_queue;
 struct semaphore mtx;
-int nr_prod_waiting,nr_cons_waiting;
+int nr_prod_waiting, nr_cons_waiting;
 
-static ssize_t prodcons_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
-{
-	char kbuf[MAX_CHARS_KBUF+1];
-	int val=0;
+static ssize_t prodcons_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+    char kbuf[MAX_CHARS_KBUF + 1];
+    int val = 0;
 
-	if ((*off) > 0) /* The application can write in this entry just once !! */
-		return 0;
+    // The application can only write in this entry once
+    if ((*off) > 0) {
+        return 0;
+    }
 
-	if (len > MAX_CHARS_KBUF) {
-		return -ENOSPC;
-	}
-	if (copy_from_user( kbuf, buf, len )) {
-		return -EFAULT;
-	}
+    if (len > MAX_CHARS_KBUF) {
+        return -ENOSPC;
+    }
 
-	kbuf[len] ='\0';
-	*off+=len;            /* Update the file pointer */
+    if (copy_from_user(kbuf, buf, len)) {
+        return -EFAULT;
+    }
 
-	if (sscanf(kbuf,"%i",&val)!=1)
-		return -EINVAL;
+    kbuf[len] = '\0';
+    // Update the file pointer
+    *off+=len;
 
-	/* Acceso a la sección crítica */
-	if (down_interruptible(&mtx))
-		return -EINTR;
+    if (sscanf(kbuf, "%i", &val) != 1) {
+        return -EINVAL;
+    }
 
-	/* Bloquearse mientras no haya huecos en el buffer */
-	while (kfifo_is_full(&cbuf)) {
-		/* Incremento de productores esperando */
-		nr_prod_waiting++;
+    // Acceso a la sección crítica
+    if (down_interruptible(&mtx)) {
+        return -EINTR;
+    }
 
-		/* Liberar el 'mutex' antes de bloqueo*/
-		up(&mtx);
+    // Bloquearse mientras no haya huecos en el buffer
+    while (kfifo_is_full(&cbuf)) {
+        // Incremento de productores esperando
+        nr_prod_waiting++;
 
-		/* Bloqueo en cola de espera */
-		if (down_interruptible(&prod_queue)) {
-			down(&mtx);
-			nr_prod_waiting--;
-			up(&mtx);
-			return -EINTR;
-		}
+        // Liberar el 'mutex' antes de bloqueo
+        up(&mtx);
 
-		/* Readquisición del 'mutex' antes de entrar a la SC */
-		if (down_interruptible(&mtx)) {
-			return -EINTR;
-		}
-	}
+        // Bloqueo en cola de espera
+        if (down_interruptible(&prod_queue)) {
+            down(&mtx);
+            nr_prod_waiting--;
+            up(&mtx);
+            return -EINTR;
+        }
 
-	/* Insertar en el buffer */
-	kfifo_in(&cbuf,&val,sizeof(int));
+        // Readquisición del 'mutex' antes de entrar a la SC
+        if (down_interruptible(&mtx)) {
+            return -EINTR;
+        }
+    }
 
-	/* Despertar a los productores bloqueados (si hay alguno) */
-	if (nr_cons_waiting>0) {
-		up(&cons_queue);
-		nr_cons_waiting--;
-	}
+    // Insertar en el buffer
+    kfifo_in(&cbuf, &val, sizeof(int));
 
-	/* Salir de la sección crítica */
-	up(&mtx);
+    // Despertar a los productores bloqueados (si hay alguno)
+    if (nr_cons_waiting > 0) {
+        up(&cons_queue);
+        nr_cons_waiting--;
+    }
 
-	return len;
+    // Salir de la sección crítica
+    up(&mtx);
+
+    return len;
 }
 
 
-static ssize_t prodcons_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
-{
-	int nr_bytes=0;
-	int bytes_extracted;
-	int val;
-	char kbuff[32]="";
+static ssize_t prodcons_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+    int nr_bytes = 0;
+    int bytes_extracted;
+    int val;
+    char kbuff[32] = "";
 
-	if ((*off) > 0)
-		return 0;
+    if ((*off) > 0) {
+        return 0;
+    }
 
-	/* Entrar a la sección crítica */
-	if (down_interruptible(&mtx)) {
-		return -EINTR;
-	}
+    // Entrar a la sección crítica
+    if (down_interruptible(&mtx)) {
+        return -EINTR;
+    }
 
-	/* Bloquearse mientras buffer esté vacío (no haya un entero) */
-	while (kfifo_len(&cbuf)<sizeof(int)) {
-		/* Incremento de consumidores esperando */
-		nr_cons_waiting++;
+    // Bloquearse mientras buffer esté vacío (no haya un entero)
+    while (kfifo_len(&cbuf) < sizeof(int)) {
+        // Incremento de consumidores esperando
+        nr_cons_waiting++;
 
-		/* Liberar el 'mutex' antes de bloqueo*/
-		up(&mtx);
+        // Liberar el 'mutex' antes de bloqueo
+        up(&mtx);
 
-		/* Bloqueo en cola de espera */
-		if (down_interruptible(&cons_queue)) {
-			down(&mtx);
-			nr_cons_waiting--;
-			up(&mtx);
-			return -EINTR;
-		}
+        // Bloqueo en cola de espera
+        if (down_interruptible(&cons_queue)) {
+            down(&mtx);
+            nr_cons_waiting--;
+            up(&mtx);
+            return -EINTR;
+        }
 
-		/* Readquisición del 'mutex' antes de entrar a la SC */
-		if (down_interruptible(&mtx)) {
-			return -EINTR;
-		}
-	}
+        // Readquisición del 'mutex' antes de entrar a la SC
+        if (down_interruptible(&mtx)) {
+            return -EINTR;
+        }
+    }
 
-	/* Extraer el primer entero del buffer */
-	bytes_extracted=kfifo_out(&cbuf,&val,sizeof(int));
+    // Extraer el primer entero del buffer
+    bytes_extracted = kfifo_out(&cbuf, &val, sizeof(int));
 
-	/* Despertar a los consumidores bloqueados (si hay alguno) */
-	if (nr_prod_waiting>0) {
-		up(&prod_queue);
-		nr_prod_waiting--;
-	}
+    // Despertar a los consumidores bloqueados (si hay alguno)
+    if (nr_prod_waiting > 0) {
+        up(&prod_queue);
+        nr_prod_waiting--;
+    }
 
-	/* Salir de la sección crítica */
-	up(&mtx);
+    // Salir de la sección crítica
+    up(&mtx);
 
-	if (bytes_extracted!=sizeof(int))
-		return -EINVAL;
+    if (bytes_extracted != sizeof(int)) {
+        return -EINVAL;
+    }
 
-	nr_bytes=sprintf(kbuff,"%i\n",val);
+    nr_bytes = sprintf(kbuff, "%i\n", val);
 
-	if (len < nr_bytes)
-		return -ENOSPC;
+    if (len < nr_bytes) {
+        return -ENOSPC;
+    }
 
-	if (copy_to_user(buf,kbuff,nr_bytes))
-		return -EFAULT;
+    if (copy_to_user(buf,kbuff,nr_bytes)) {
+        return -EFAULT;
+    }
 
-	(*off)+=nr_bytes;  /* Update the file pointer */
+    (*off) += nr_bytes;
 
-	return nr_bytes;
+    return nr_bytes;
 }
 
 static const struct file_operations proc_entry_fops = {
-	.read = prodcons_read,
-	.write = prodcons_write,
+    .read = prodcons_read,
+    .write = prodcons_write,
 };
 
-int init_prodcons_module( void )
-{
-	int retval;
-	/* Inicialización del buffer */
-	retval = kfifo_alloc(&cbuf,MAX_ITEMS_CBUF*sizeof(int),GFP_KERNEL);
+int init_prodcons_module(void) {
+    int retval;
+    // Inicialización del buffer
+    retval = kfifo_alloc(&cbuf, MAX_ITEMS_CBUF * sizeof(int), GFP_KERNEL);
 
-	if (retval)
-		return -ENOMEM;
+    if (retval) {
+        return -ENOMEM;
+    }
 
-	/* Inicialización a 0 de los semáforos usados como colas de espera */
-	sema_init(&prod_queue,0);
-	sema_init(&cons_queue,0);
+    // Inicialización a 0 de los semáforos usados como colas de espera
+    sema_init(&prod_queue, 0);
+    sema_init(&cons_queue, 0);
 
-	/* Inicializacion a 1 del semáforo que permite acceso en exclusión mutua a la SC */
-	sema_init(&mtx,1);
+    // Inicializacion a 1 del semáforo que permite acceso en exclusión mutua a la SC
+    sema_init(&mtx, 1);
 
-	nr_prod_waiting=nr_cons_waiting=0;
+    nr_prod_waiting = nr_cons_waiting = 0;
 
-	proc_entry = proc_create_data("prodcons",0666, NULL, &proc_entry_fops, NULL);
+    proc_entry = proc_create_data("prodcons", 0666, NULL, &proc_entry_fops, NULL);
 
-	if (proc_entry == NULL) {
-		kfifo_free(&cbuf);
-		printk(KERN_INFO "Prodcons2: No puedo crear la entrada en proc\n");
-		return  -ENOMEM;
-	}
+    if (proc_entry == NULL) {
+        kfifo_free(&cbuf);
+        printk(KERN_INFO "Prodcons2: No puedo crear la entrada en proc\n");
+        return -ENOMEM;
+    }
 
-	printk(KERN_INFO "Prodcons2: Cargado el Modulo.\n");
+    printk(KERN_INFO "Prodcons2: Cargado el Modulo.\n");
 
-	return 0;
+    return 0;
 }
 
 
-void exit_prodcons_module( void )
-{
-	remove_proc_entry("prodcons", NULL);
-	kfifo_free(&cbuf);
-	printk(KERN_INFO "Prodcons2: Modulo descargado.\n");
+void exit_prodcons_module(void) {
+    remove_proc_entry("prodcons", NULL);
+    kfifo_free(&cbuf);
+    printk(KERN_INFO "Prodcons2: Modulo descargado.\n");
 }
 
-
-module_init( init_prodcons_module );
-module_exit( exit_prodcons_module );
+module_init(init_prodcons_module);
+module_exit(exit_prodcons_module);
